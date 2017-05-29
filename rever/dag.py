1340fc7f3dcf386a5bbb2c97c084ef09e45abb05
Anthony Scopatz <scopatz@gmail.com>
2017-05-29 13:14:42 -0400

    added dependcies already done tracking to DAG solver

--- rever/dag.py
+++ rever/dag.py
@@ -1,7 +1,7 @@
 """Tools for dealing with activity DAGs."""
 
 
-def find_path(dag, ends, path=None):
+def find_path(dag, ends, done=frozenset(), path=None, already_done=None):
     """Returns a list that includes all end points for a given DAG.
 
     Parameters
@@ -10,26 +10,42 @@ def find_path(dag, ends, path=None):
         A DAG of all possible activities
     ends : set of str
         End points to compute.
+    done : set of str, optional
+        The nodes that have already been computed
     path : list of str, optional
         This is the return value of which activities to execute in which order.
+    already_done : list of str
+        The return value  of the activities that have already been computed
+        that would otherwise need to be computed.
 
     Returns
     -------
     path : list of str
         The path through the DAG.
+    already_done : list of str
+        Activities that have already been computed that would otherwise need to
+        be computed.
     """
     if path is None:
         path = []
-    sofar = set(path)
+    if not isinstance(done, frozenset):
+        done = frozenset(done)
+    if already_done is None:
+        already_done = []
+    pth = set(path)
+    sofar = pth | done
     todo = ends - sofar
+    would_do = (ends - pth) & done
+    already_done.extend([x for x in sorted(would_do) if x not in already_done])
     if len(todo) == 0:
-        return path
+        return path, already_done
     for end in sorted(todo):
         deps = set(dag[end].deps)
         need = deps - sofar
+        already_done.extend([x for x in sorted(deps & done) if x not in already_done])
         if len(need) > 0:
-            find_path(dag, need, path)
+            find_path(dag, need, done=done, path=path, already_done=already_done)
             sofar.update(path)
         if end not in sofar:
             path.append(end)
-    return path
+    return path, already_done
--- tests/test_dag.py
+++ tests/test_dag.py
@@ -4,55 +4,123 @@ from rever.activity import Activity
 
 
 def test_empty():
-    path = find_path({}, set())
+    path, _ = find_path({}, set())
     assert [] == path
 
 
 def test_single():
-    path = find_path({'a': Activity()}, {'a'})
+    path, _ = find_path({'a': Activity()}, {'a'})
     assert ['a'] == path
 
 
 def test_two_unrelated():
     dag = {'a': Activity(), 'b': Activity()}
-    path = find_path(dag, {'a', 'b'})
+    path, _ = find_path(dag, {'a', 'b'})
     assert ['a', 'b'] == path
 
 
 def test_two_dep():
     dag = {'a': Activity(), 'b': Activity(deps=set('a'))}
-    path = find_path(dag, {'b'})
+    path, _ = find_path(dag, {'b'})
     assert ['a', 'b'] == path
 
 
 def test_three_linear():
     dag = {'a': Activity(), 'b': Activity(deps=set('a')), 'c': Activity(deps=set('b'))}
-    path = find_path(dag, {'c'})
+    path, _ = find_path(dag, {'c'})
     assert ['a', 'b', 'c'] == path
 
 
 def test_three_forked():
     dag = {'a': Activity(), 'b': Activity(deps=set('a')), 'c': Activity(deps=set('a'))}
-    path = find_path(dag, {'b'})
+    path, _ = find_path(dag, {'b'})
     assert ['a', 'b'] == path
-    path = find_path(dag, {'c'})
+    path, _ = find_path(dag, {'c'})
     assert ['a', 'c'] == path
-    path = find_path(dag, {'b', 'c'})
+    path, _ = find_path(dag, {'b', 'c'})
     assert ['a', 'b', 'c'] == path
 
 
 def test_five_forked():
     dag = {'a': Activity(), 'b': Activity(deps=set('a')), 'c': Activity(deps=set('b')),
            'd': Activity(deps=set('a')), 'e': Activity(deps=set('c'))}
-    path = find_path(dag, {'d'})
+    path, _ = find_path(dag, {'d'})
     assert ['a', 'd'] == path
-    path = find_path(dag, {'e'})
+    path, _ = find_path(dag, {'e'})
     assert ['a', 'b', 'c', 'e'] == path
-    path = find_path(dag, {'d', 'e'})
+    path, _ = find_path(dag, {'d', 'e'})
     assert ['a', 'd', 'b', 'c', 'e'] == path
-    path = find_path(dag, {'b', 'c', 'd', 'e'})
+    path, _ = find_path(dag, {'b', 'c', 'd', 'e'})
     assert ['a', 'b', 'c', 'd', 'e'] == path
 
 
+def test_empty_done():
+    path, already_done = find_path({}, set(), done={'a'})
+    assert [] == path
+    assert [] == already_done
 
 
+def test_single_done():
+    path, already_done = find_path({'a': Activity()}, {'a'}, done={'a'})
+    assert [] == path
+    assert ['a'] == already_done
+
+
+def test_two_unrelated_done():
+    dag = {'a': Activity(), 'b': Activity()}
+    path, already_done = find_path(dag, {'a', 'b'}, done={'a'})
+    assert ['b'] == path
+    assert ['a'] == already_done
+
+
+def test_two_dep_done():
+    dag = {'a': Activity(), 'b': Activity(deps=set('a'))}
+    path, already_done = find_path(dag, {'b'}, done={'a'})
+    assert ['b'] == path
+    assert ['a'] == already_done
+
+
+def test_three_linear_done():
+    dag = {'a': Activity(), 'b': Activity(deps=set('a')), 'c': Activity(deps=set('b'))}
+    # just a is done
+    path, already_done = find_path(dag, {'c'}, done={'a'})
+    assert ['b', 'c'] == path
+    assert ['a'] == already_done
+    # b is done, which implies that a is done
+    path, already_done = find_path(dag, {'c'}, done={'b'})
+    assert ['c'] == path
+    assert ['b'] == already_done
+    # a and b are done, only b matters
+    path, already_done = find_path(dag, {'c'}, done={'a', 'b'})
+    assert ['c'] == path
+    assert ['b'] == already_done
+
+
+def test_three_forked_done():
+    dag = {'a': Activity(), 'b': Activity(deps=set('a')), 'c': Activity(deps=set('a'))}
+    path, already_done = find_path(dag, {'b'}, done={'a'})
+    assert ['b'] == path
+    assert ['a'] == already_done
+    path, already_done = find_path(dag, {'c'}, done={'a'})
+    assert ['c'] == path
+    assert ['a'] == already_done
+    path, already_done = find_path(dag, {'b', 'c'}, done={'a'})
+    assert ['b', 'c'] == path
+    assert ['a'] == already_done
+
+
+def test_five_forked_done():
+    dag = {'a': Activity(), 'b': Activity(deps=set('a')), 'c': Activity(deps=set('b')),
+           'd': Activity(deps=set('a')), 'e': Activity(deps=set('c'))}
+    path, already_done = find_path(dag, {'d'}, done={'a'})
+    assert ['d'] == path
+    assert ['a'] == already_done
+    path, already_done = find_path(dag, {'e'}, done={'b'})
+    assert ['c', 'e'] == path
+    assert ['b'] == already_done
+    path, already_done = find_path(dag, {'d', 'e'}, done={'a', 'b'})
+    assert ['d', 'c', 'e'] == path
+    assert ['a', 'b'] == already_done
+    path, already_done = find_path(dag, {'b', 'c', 'd', 'e'}, done={'a', 'c'})
+    assert ['b', 'd', 'e'] == path
+    assert ['c', 'a'] == already_done

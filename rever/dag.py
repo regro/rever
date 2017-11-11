6dea4f05b7c90f2bb3c50c7fcfcdc4f3455dc418
Anthony Scopatz <scopatz@gmail.com>
2017-11-11 18:02:26 -0500

    fix for defaultdict

--- rever/dag.py
+++ rever/dag.py
@@ -40,9 +40,11 @@ def find_path(dag, ends, done=frozenset(), path=None, already_done=None):
     if len(todo) == 0:
         return path, already_done
     for end in sorted(todo):
-        if end not in dag:
+        try:
+            deps = set(dag[end].deps)
+        except KeyError:
+            # we need to do it this way to support defaultdict
             raise KeyError('{0!r} not found in DAG!'.format(end))
-        deps = set(dag[end].deps)
         need = deps - sofar
         already_done.extend([x for x in sorted(deps & done) if x not in already_done])
         if len(need) > 0:

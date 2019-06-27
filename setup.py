bfb6f32f3994c23512c6104678a597804bf8fccb
Anthony Scopatz <scopatz@gmail.com>
2019-06-27 13:33:45 -0400

    bumped version to 0.3.8

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def main():
         description='Release Versions of Software',
         long_description=readme,
         license='BSD',
-        version='0.3.7',
+        version='0.3.8',
         author='Anthony Scopatz',
         maintainer='Anthony Scopatz',
         author_email='scopatz@gmail.com',

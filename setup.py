074e1488d9c9e7cbd5b905830245f759df0c59fc
Thomas Kluyver <thomas@kluyver.me.uk>
2019-10-24 13:19:52 +0100

    rever also requires lazyasd

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def main():
         package_data={'rever': ['*.xsh'], 'rever.activities': ['*.xsh']},
         scripts=scripts,
         zip_safe=False,
-        install_requires=['xonsh'],
+        install_requires=['xonsh', 'lazyasd'],
         )
     # WARNING!!! Do not use setuptools 'console_scripts'
     # It validates the depenendcies everytime the
